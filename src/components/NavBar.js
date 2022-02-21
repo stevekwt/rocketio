cad44f6fceb200920d6fcbc729c40625dfbde931
Stephen Thomas <66618292+stevekwt@users.noreply.github.com>
2022-02-20 17:33:26 -0800

    alt tags added, no MQs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,32 +8,21 @@ import {makeStyles} from '@material-ui/core/styles'
 const styles = makeStyles({
     bar:{
         paddingTop: "1.15rem",
-        backgroundColor: "#fff",
-        ['@media (max-width:780px)']: { 
-           flexDirection: "column"
-          }
+        backgroundColor: "#fff"
     },
     logo: {
-        width: "15%", 
-        ['@media (max-width:780px)']: { 
-           display: "none"
-           }
+        width: "15%"
     },
     logoMobile:{
         width: "100%", 
-        display: "none", 
-        ['@media (max-width:780px)']: { 
-            display: "inline-block"
-            }
+        display: "none"
     },
     menuItem: {
         cursor: "pointer", 
         flexGrow: 1,
         "&:hover": {
             color:  "#4f25c8"
-        },
-        ['@media (max-width:780px)']: { 
-            paddingBottom: "1rem"    }
+        }
     }
 })
 
@@ -41,8 +30,8 @@ function NavBar() {
     const classes = styles()
     return ( 
         <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-            <img src={logo} className={classes.logo}/> 
-            <img src={logoMobile} className={classes.logoMobile}/> 
+            <img src={logo} alt="Rocketio" className={classes.logo}/> 
+            <img src={logoMobile} alt="Rocketio" className={classes.logoMobile}/> 
             <Typography variant="h6" className={classes.menuItem}>
                 About
             </Typography>
